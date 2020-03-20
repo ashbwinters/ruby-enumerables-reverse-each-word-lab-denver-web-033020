de332a623bb386071b1095faac75706e83a26ee0
Ashley Winters <ashbwinters@gmail.com>
2020-03-20 22:59:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   p array = [string.split]
-  p array.collect{|word|("#{word}".reverse)}
-  p array.join(' ')
+  p new array = array.collect{|word|("#{word}".reverse)}
+  p new_array.join(' ')
 end
